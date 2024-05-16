00c9f7968bf14389259c52c1548a0de9ebf210be
William Vasquez <william.a.vasquez@gmail.com>
2024-05-16 14:19:57 -0700

    deploying with pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://williamavasquez.github.io",
+  base: "kcgs",
   integrations: [tailwind(), mdx(), sitemap()],
 });
